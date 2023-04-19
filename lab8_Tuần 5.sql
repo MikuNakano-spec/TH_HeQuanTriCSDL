95625b3af6d866c1e5420d4204a344465cb21f99
Võ Thanh Nghị <80967790+MikuNakano-spec@users.noreply.github.com>
2023-04-19 09:01:42 +0700

    Add files via upload

---
+++ lab8_Tuần 5.sql
@@ -0,0 +1,282 @@
+﻿CREATE PROCEDURE lab8_cau1
+    @manv nchar(10), 
+    @tennv nvarchar(20), 
+    @gioitinh nvarchar(10), 
+    @diachi nvarchar(30), 
+    @sodt nvarchar(20), 
+    @email nvarchar(30), 
+    @phong nvarchar(30), 
+    @flag int
+AS
+BEGIN
+    IF (@flag = 0)
+    BEGIN
+        IF (@gioitinh <> 'Nam' AND @gioitinh <> 'Nữ')
+        BEGIN
+            SELECT 1 AS 'MaLoi' 
+            RETURN
+        END
+        
+        INSERT INTO Nhanvien (manv, tennv, gioitinh, diachi, sodt, email, phong)
+        VALUES (@manv, @tennv, @gioitinh, @diachi, @sodt, @email, @phong)
+        
+        SELECT 0 AS 'MaLoi' 
+        RETURN
+    END
+    
+    IF (@flag = 1) 
+    BEGIN
+        IF (@gioitinh <> 'Nam' AND @gioitinh <> 'Nữ')
+        BEGIN
+            SELECT 1 AS 'MaLoi'
+            RETURN
+        END
+        
+        UPDATE Nhanvien 
+        SET tennv = @tennv, 
+            gioitinh = @gioitinh, 
+            diachi = @diachi, 
+            sodt = @sodt, 
+            email = @email,
+            phong = @phong
+        WHERE manv = @manv
+        
+        SELECT 0 AS 'MaLoi' 
+        RETURN
+    END
+END
+
+CREATE PROCEDURE lab8_cau2 
+    @masp nchar(10), 
+    @tenhang nvarchar(20), 
+    @tensp nvarchar(20), 
+    @soluong int, 
+    @mausac nvarchar(20), 
+    @giaban money, 
+    @donvitinh nchar(10), 
+    @mota nvarchar(MAX), 
+    @flag int
+AS
+BEGIN
+    IF (@flag = 0) 
+    BEGIN
+        IF NOT EXISTS (SELECT * FROM Hangsx WHERE tenhang = @tenhang)
+        BEGIN
+            SELECT 1 AS 'MaLoi'
+            RETURN
+        END
+        
+        IF (@soluong < 0)
+        BEGIN
+            SELECT 2 AS 'MaLoi' 
+            RETURN
+        END
+
+        INSERT INTO Sanpham (masp, mahangsx, tensp, soluong, mausac, giaban, donvitinh, mota)
+        VALUES (@masp, (SELECT mahangsx FROM Hangsx WHERE tenhang = @tenhang), @tensp, @soluong, @mausac, @giaban, @donvitinh, @mota)
+        
+        SELECT 0 AS 'MaLoi' 
+        RETURN
+    END
+    
+    IF (@flag = 1)
+    BEGIN
+        IF NOT EXISTS (SELECT * FROM Hangsx WHERE tenhang = @tenhang)
+        BEGIN
+            SELECT 1 AS 'MaLoi'
+            RETURN
+        END
+        
+        IF (@soluong < 0)
+        BEGIN
+            SELECT 2 AS 'MaLoi' 
+            RETURN
+        END
+        
+        UPDATE Sanpham 
+        SET mahangsx = (SELECT mahangsx FROM Hangsx WHERE tenhang = @tenhang), 
+            tensp = @tensp, 
+            soluong = @soluong, 
+            mausac = @mausac, 
+            giaban = @giaban, 
+            donvitinh = @donvitinh, 
+            mota = @mota
+        WHERE masp = @masp
+        
+        SELECT 0 AS 'MaLoi' 
+        RETURN
+    END
+END
+
+CREATE PROCEDURE lab8_cau3
+	@manv nchar(10)
+AS
+BEGIN
+    IF NOT EXISTS (SELECT * FROM Nhanvien WHERE manv = @manv)
+    BEGIN
+        SELECT 1 AS 'MaLoi' 
+        RETURN
+    END
+
+    BEGIN TRANSACTION 
+
+    DELETE FROM Nhap WHERE manv = @manv
+
+    DELETE FROM Xuat WHERE manv = @manv
+
+    DELETE FROM Nhanvien WHERE manv = @manv
+
+    IF @@ERROR <> 0 
+    BEGIN
+        ROLLBACK
+        SELECT 1 AS 'MaLoi' 
+        RETURN
+    END
+
+    COMMIT 
+    SELECT 0 AS 'MaLoi' 
+END
+
+CREATE PROCEDURE lab8_cau4
+	@masp nchar(10)
+AS
+BEGIN
+    IF NOT EXISTS (SELECT * FROM Sanpham WHERE masp = @masp)
+    BEGIN
+        SELECT 1 AS 'MaLoi' 
+        RETURN
+    END
+
+    BEGIN TRANSACTION 
+
+    DELETE FROM Nhap WHERE masp = @masp
+
+    DELETE FROM Xuat WHERE masp = @masp
+
+    DELETE FROM Sanpham WHERE masp = @masp
+
+    IF @@ERROR <> 0 
+    BEGIN
+        ROLLBACK
+        SELECT 1 AS 'MaLoi' 
+        RETURN
+    END
+
+    COMMIT 
+    SELECT 0 AS 'MaLoi' 
+END
+
+CREATE PROCEDURE lab8_cau5
+	@mahangsx nchar(10),
+	@tenhang nvarchar(20),
+	@diachi nvarchar(30),
+	@sodt nvarchar(20),
+	@email nvarchar(30)
+AS
+BEGIN
+	IF NOT EXISTS (SELECT * FROM Hangsx where tenhang = @tenhang)
+	BEGIN
+		SELECT 0 AS 'MaLoi'
+		RETURN;
+	END
+
+	INSERT INTO Hangsx (mahangsx, tenhang, diachi, sodt, email)
+	VALUES (@mahangsx, @tenhang, @diachi, @sodt, @email);
+
+	IF EXISTS (SELECT * FROM Hangsx where tenhang = @tenhang)
+	BEGIN
+		SELECT 1 AS 'MaLoi'
+		RETURN
+	END
+END
+
+CREATE PROCEDURE lab8_cau6
+	@sohdn nchar(10),
+    @masp nchar(10),
+    @manv nchar(10),
+    @ngaynhap datetime,
+    @soluongN int,
+    @dongiaN money
+AS
+BEGIN
+	 IF NOT EXISTS (SELECT * FROM Sanpham WHERE masp = @masp)
+	 BEGIN
+		SELECT 1 AS 'MaLoi'
+		RETURN;
+	END	
+
+	 IF NOT EXISTS (SELECT * FROM Nhanvien WHERE manv = @manv)
+	 BEGIN
+		SELECT 2 AS 'MaLoi'
+		RETURN;
+	END	
+
+	IF EXISTS (SELECT 1 FROM Nhap WHERE sohdn = @sohdn)
+        UPDATE Nhap
+        SET masp = @masp,
+            manv = @manv,
+            ngaynhap = @ngaynhap,
+            soluongN = @soluongN,
+            dongiaN = @dongiaN
+        WHERE sohdn = @sohdn
+    ELSE
+        INSERT INTO Nhap (sohdn, masp, manv, ngaynhap, soluongN, dongiaN)
+        VALUES (@sohdn, @masp, @manv, @ngaynhap, @soluongN, @dongiaN)
+
+    SELECT 0 AS 'MaLoi'
+	RETURN
+END
+
+CREATE PROCEDURE lab8_cau7
+    @sohdx nchar(10), 
+    @masp nchar(10), 
+    @manv nchar(10), 
+    @ngayxuat datetime, 
+    @soluongX int
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    
+    IF NOT EXISTS(SELECT * FROM Sanpham WHERE masp = @masp)
+    BEGIN
+        SELECT 1 AS 'MaLoi' 
+        RETURN;
+    END
+
+   
+    IF NOT EXISTS(SELECT * FROM Nhanvien WHERE manv = @manv)
+    BEGIN
+        SELECT 2 AS 'MaLoi'
+        RETURN;
+    END
+
+    
+    IF @soluongX > (SELECT SoLuong FROM Sanpham WHERE masp = @masp)
+    BEGIN
+        SELECT 3 AS 'MaLoi'
+        RETURN;
+    END
+
+
+    IF EXISTS(SELECT * FROM Xuat WHERE sohdx = @sohdx)
+    BEGIN
+      
+        UPDATE Xuat 
+        SET masp = @masp, 
+            manv = @manv, 
+            ngayxuat = @ngayxuat, 
+            soluongX = @soluongX 
+        WHERE sohdx = @sohdx;
+    END
+    ELSE
+    BEGIN
+      
+        INSERT INTO Xuat (sohdx, masp, manv, ngayxuat, soluongX) 
+        VALUES (@sohdx, @masp, @manv, @ngayxuat, @soluongX);
+    END
+
+    
+    SELECT 0 AS 'MaLoi'
+	RETURN
+END
---
+++ lab9_Tuần 5.sql
@@ -0,0 +1,162 @@
+﻿
+CREATE TRIGGER lab9_cau1
+ON Nhap
+FOR INSERT
+AS
+BEGIN
+    DECLARE @masp NVARCHAR(10), @manv NVARCHAR(10)
+    DECLARE @sln INT, @dgn FLOAT
+
+    SELECT @masp = masp, @manv = manv, @sln = soluongN, @dgn = dongiaN
+    FROM inserted
+
+    IF NOT EXISTS (SELECT * FROM sanpham WHERE masp = @masp)
+    BEGIN
+        RAISERROR(N'Không tồn tại sản phẩm trong danh mục sản phẩm.', 16, 1)
+        ROLLBACK TRANSACTION
+    END
+    ELSE IF NOT EXISTS (SELECT * FROM nhanvien WHERE manv = @manv)
+    BEGIN
+        RAISERROR(N'Không tồn tại nhân viên có mã này.', 16, 1)
+        ROLLBACK TRANSACTION
+    END
+    ELSE IF (@sln <= 0 OR @dgn <= 0)
+    BEGIN
+        RAISERROR(N'Nhập sai số lượng hoặc đơn giá.', 16, 1)
+        ROLLBACK TRANSACTION
+    END
+    ELSE
+    BEGIN
+        UPDATE Sanpham
+        SET soluong = soluong + @sln
+        WHERE masp = @masp
+    END
+END
+
+
+CREATE TRIGGER lab9_cau2
+ON Xuat
+FOR INSERT
+AS
+BEGIN
+    DECLARE @masp nvarchar(10), @manv nvarchar(10)
+    DECLARE @slx int, @dgb float
+    SELECT @masp = masp, @manv = manv, @slx = soluongX, @dgb = dongiaX FROM inserted
+    IF NOT EXISTS (SELECT * FROM SanPham WHERE masp = @masp)
+    BEGIN
+        RAISERROR(N'Sản phẩm không tồn tại trong danh mục sản phẩm.', 16, 1)
+        ROLLBACK TRANSACTION
+    END
+    ELSE IF NOT EXISTS (SELECT * FROM NhanVien WHERE manv = @manv)
+    BEGIN
+        RAISERROR(N'Nhân viên không tồn tại trong danh mục nhân viên.', 16, 1)
+        ROLLBACK TRANSACTION
+    END
+    ELSE IF (@slx <= 0 OR @dgb <= 0)
+    BEGIN
+        RAISERROR(N'Số lượng hoặc đơn giá xuất không hợp lệ.', 16, 1)
+        ROLLBACK TRANSACTION
+    END
+    ELSE IF (@slx > (SELECT soluong FROM SanPham WHERE masp = @masp))
+    BEGIN
+        RAISERROR(N'Số lượng sản phẩm trong kho không đủ để xuất.', 16, 1)
+        ROLLBACK TRANSACTION
+    END
+    ELSE
+    BEGIN
+        UPDATE SanPham SET soluong = soluong - @slx WHERE masp = @masp
+    END
+END
+
+
+CREATE TRIGGER lab9_cau3
+ON Xuat
+AFTER DELETE
+AS
+BEGIN
+  DECLARE @masp NVARCHAR(10), @sln INT;
+
+  SELECT @masp = d.masp, @sln = d.soluongX
+  FROM deleted d;
+
+  UPDATE Sanpham
+  SET soluong = soluong + @sln
+  WHERE masp = @masp;
+END
+
+
+CREATE TRIGGER lab9_cau4
+ON Xuat
+AFTER UPDATE
+AS
+BEGIN
+    DECLARE @Count INT, @masp NVARCHAR(10), @sln INT, @sln_old INT
+
+    SELECT @Count = COUNT(*) FROM INSERTED
+
+    IF @Count > 1
+    BEGIN
+        RAISERROR(N'Số bản ghi thay đổi > 1 bản ghi', 16, 1)
+        ROLLBACK TRANSACTION
+        RETURN
+    END
+
+    SELECT @masp = i.masp, @sln = i.soluongX, @sln_old = d.soluongX
+    FROM INSERTED i INNER JOIN DELETED d ON i.sohdx = d.sohdx AND i.masp = d.masp
+
+    IF @sln < @sln_old
+    BEGIN
+        RAISERROR(N'Số lượng xuất thay đổi nhỏ hơn số lượng trong bảng sản phẩm', 16, 1)
+        ROLLBACK TRANSACTION
+        RETURN
+    END
+
+    UPDATE Xuat SET soluongX = @sln WHERE sohdx = (SELECT sohdx FROM INSERTED)
+
+    UPDATE Sanpham SET soluong = soluong + (@sln_old - @sln) WHERE masp = @masp
+
+END
+
+
+CREATE TRIGGER lab9_cau5
+ON Nhap
+AFTER UPDATE
+AS
+BEGIN
+    IF (SELECT COUNT(*) FROM inserted) > 1
+    BEGIN
+        RAISERROR(N'Chỉ được phép cập nhật một bản ghi tại một thời điểm!', 16, 1)
+        ROLLBACK TRANSACTION
+    END
+    ELSE
+    BEGIN
+        DECLARE @masp NVARCHAR(10), @sln_old INT, @sln_new INT
+        
+        SELECT @masp = i.masp, @sln_old = d.soluongN, @sln_new = i.soluongN
+        FROM inserted i
+        INNER JOIN deleted d ON i.masp = d.masp
+        
+        IF @sln_new < @sln_old
+        BEGIN
+            RAISERROR(N'Số lượng nhập mới phải lớn hơn số lượng cũ!', 16, 1)
+            ROLLBACK TRANSACTION
+        END
+        ELSE
+        BEGIN
+            UPDATE Sanpham
+            SET soluong = soluong + (@sln_new - @sln_old)
+            WHERE masp = @masp
+        END
+    END
+END
+
+
+CREATE TRIGGER lab9_cau6
+ON Nhap
+AFTER DELETE
+AS
+BEGIN
+    DECLARE @masp nvarchar(10), @sln int
+    SELECT @masp = masp, @sln = soluongN FROM deleted
+    UPDATE Sanpham SET soluong = soluong - @sln WHERE masp = @masp
+END
